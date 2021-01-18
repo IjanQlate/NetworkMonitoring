cdca1f5e88047513ce3309152b66bff884261740
IjanQlate <mat.ijan89@gmail.com>
2021-01-18 14:06:04 +0800

    update

--- python/remotecontrol/shutdown.py
+++ python/remotecontrol/shutdown.py
@@ -1,7 +1,11 @@
 import os, sys
 
-command = "shutdown /s /t 1 -m \\{}".format(sys.argv[1])
-
+command = "shutdown /s /t 1 -m \\\\{}".format(sys.argv[1])
 # print (command)
-
 os.system(command)
+
+if os.system(command) is not "":
+    # print(os.system(command))
+    print(os.WEXITSTATUS(command))
+
+# print (os.system(command))
