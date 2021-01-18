08e0b7ed49e281910cfc06c29ac0db786737156a
famierarara <cikfamieramelati@gmail.com>
2021-01-18 12:25:54 +0800

    update python command

--- database/remotecontrol.php
+++ database/remotecontrol.php
@@ -1,6 +1,7 @@
 <?php
 // echo "hey";
 $command = escapeshellcmd('python ../python/logout.py');
+// $command = escapeshellcmd('python get_tmsoa.py '.$_POST['service_number']);
 $output = shell_exec($command);
 echo $output;
 
--- python/shutdown.py
+++ python/shutdown.py
@@ -6,3 +6,4 @@ if shutdown == 'no':
     exit() 
 else: 
     os.system("shutdown /s /t 1") 
+    # os.system("shutdown -l -m \\192.168.10.111") 
