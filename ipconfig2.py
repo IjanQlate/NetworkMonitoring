da2f7ea4599575612ef3cc212ecdece29521d2d4
Famiera Atika <cikfamieramelati@gmail.com>
2021-01-22 16:31:37 +0800

    Add files via upload

---
+++ ipconfig2.py
@@ -0,0 +1,59 @@
+import subprocess
+import re
+import pprint
+
+
+output = subprocess.check_output("ipconfig /all")
+
+lines = output.splitlines()
+pp = pprint.PrettyPrinter()
+# pp.pprint(lines)
+
+lines = filter(lambda x: x, lines)
+
+
+
+items_dict = {}
+header_list = []
+
+# Get Header
+count = 0
+for line in lines:
+    line = str(line)
+    
+    # print(line)
+    num = line.find(".")
+    # print(num)
+    if num<0:  
+        count = int(count)      
+        header_list.append(line)
+        head = line
+        count = count + 1
+        # print(line)
+        # print(count)
+    else:
+        count = str(count)
+        items_dict[count,line] = head
+
+        
+# pp.pprint(items_dict)
+
+
+user_input = "Bluetooth"
+# user_input = "Ethernet adapter Local Area Connection" 
+# user_input = "Ethernet adapter Ethernet"
+user_input = "Wireless LAN adapter Wi-Fi"
+user_input = "Wireless LAN adapter Local Area Connection"
+user_input = "IP Configuration"
+
+
+
+for x in header_list:
+    if user_input in x:
+        print(x)
+        for item, header in items_dict.items():  
+            if header == x:
+                print(item)
+
+
+
